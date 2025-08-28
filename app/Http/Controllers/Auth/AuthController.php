9e050c59348cc38d7f78c6c2471fbd62c578d722
Daniel Panyin Mensah <78688545+Daniel024419@users.noreply.github.com>
2025-08-28 03:38:04 +0000

    Update AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -43,6 +43,7 @@ class AuthController extends Controller
 
         $credentials = $request->validated();
         $remember = $credentials['remember_me'] ?? false;
+        dd($request->all());
         try {
 
             $column = filter_var($credentials['identifier'], FILTER_VALIDATE_EMAIL) ? 'email' : 'phone';
